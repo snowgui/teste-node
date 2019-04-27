28607acd09dfd8bbd436a16118b1a5b26099197f
Guilherme <guilherme@smit.net.br>
2019-04-27 12:00:27 -0300

    detalhes da noticia att, ooctoo/termoscondicoes att

---
+++ app/routes/noticia.js
@@ -0,0 +1,13 @@
+module.exports = function (app) {
+
+  app.get('/noticia', function(req, res){
+
+    var connection = app.config.dbConnection();
+
+    connection.query('select * from noticias where id_noticia = 2', function(error, result){
+    
+      res.render("noticias/noticia", {noticias : result});
+
+    }); 
+  });
+};
--- app/routes/ooctoo.js
+++ app/routes/ooctoo.js
@@ -5,11 +5,7 @@ module.exports = function(app){
         var conn = app.config.dbConnectionooctoo();
 
         conn.query('select app_termos from termos_de_usos where id = 0', function(error, result){
-        
-            for(var i=0; i <result.length; i++){
             res.json(result[0].app_termos);
-        } 
-     
         });    
         
         //res.render("ooctoo/termos");
